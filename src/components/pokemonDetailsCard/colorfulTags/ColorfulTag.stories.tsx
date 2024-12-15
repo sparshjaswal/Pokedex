8307894de9a32448ade711d0a23fcadd33c305d7
sparsh jaswal <jaswalsparsh@gmail.com>
2024-12-15 17:57:09 +0530

    changes

--- src/components/pokemonDetailsCard/colorfulTags/ColorfulTag.stories.tsx
+++ src/components/pokemonDetailsCard/colorfulTags/ColorfulTag.stories.tsx
@@ -1,4 +1,3 @@
-// ColorfulTag.stories.tsx
 import React from 'react';
 import ColorfulTag from './colorfulTag';
 import { Meta, StoryFn } from '@storybook/react';
@@ -15,19 +14,19 @@ export const Default = Template.bind({});
 Default.args = {
   text: 'Fire',
   className: 'tag-fire',
-  type: 'fire', // Assuming 'fire' is a valid type for getPokemonBgColor
+  type: 'fire', 
 };
 
 export const Water = Template.bind({});
 Water.args = {
   text: 'Water',
   className: 'tag-water',
-  type: 'water', // Assuming 'water' is a valid type for getPokemonBgColor
+  type: 'water', 
 };
 
 export const Grass = Template.bind({});
 Grass.args = {
   text: 'Grass',
   className: 'tag-grass',
-  type: 'grass', // Assuming 'grass' is a valid type for getPokemonBgColor
+  type: 'grass',
 };
