7c317ac824d5bec51307ea6fd2b0d7dec4d492f8
sparsh jaswal <jaswalsparsh@gmail.com>
2024-12-16 11:46:22 +0530

    changes

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest.setup.js'], // Path to setup file
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: 'coverage', // Directory for coverage reports
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+};
