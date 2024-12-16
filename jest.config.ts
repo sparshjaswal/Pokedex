d7ee3a62516f970141bfa8af9aecc88d95a28874
sparsh jaswal <jaswalsparsh@gmail.com>
2024-12-16 15:14:57 +0530

    change

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,15 @@ import '@testing-library/jest-dom/extend-expect';
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
-  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).[jt]s?(x)'],
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'babel-jest',
   },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  setupFiles: ['<rootDir>/jest.setup.js'], // Path to setup file
-  collectCoverage: true, // Enable coverage collection
-  coverageDirectory: 'coverage', // Directory for coverage reports
+  moduleFileExtensions: ['ts', 'tsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
   moduleNameMapper: {
-    '\\.(css|less)$': 'identity-obj-proxy', // Mock CSS imports
+    '\\.(css|less)$': 'identity-obj-proxy',
   },
 };
